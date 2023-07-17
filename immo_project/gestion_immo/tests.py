ca0d850ab8c16c521db3978ff4585ca511f4db59
Lucie Pellier <lucie@luciepellier.com>
2023-07-17 19:33:13 +0200

    TDD test add apartment failed

--- immo_project/gestion_immo/tests.py
+++ immo_project/gestion_immo/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class ApartementTest(TestCase):
+    def test_add_apartment(self):
+        # Test in TDD, we check there's no apartment created
+        self.assertEqual(ApartementTest.objects.count(), 0)
